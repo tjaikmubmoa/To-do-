2c25734608c837201ff1be84568ab7a19eb90912
Takuma Jimbo <user@m-takuma-jinbo-no-macbook-pro.local>
2017-02-18 15:14:55 +0900

    Initial Commit

---
+++ To do/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  To do
+//
+//  Created by Takuma Jimbo on 2017/02/18.
+//  Copyright © 2017年 Takuma Jimbo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
